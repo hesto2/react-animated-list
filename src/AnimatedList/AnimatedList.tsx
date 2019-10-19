00c11fd8bddda993c8b34de1b1346a43a3f760de
carter.hesterman <carter.hesterman@lifeomic.com>
2019-10-18 18:16:38 -0600

    kinda working version

--- src/AnimatedList/AnimatedList.tsx
+++ src/AnimatedList/AnimatedList.tsx
@@ -1,25 +1,26 @@
 import * as React from "react";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Grow from "@material-ui/core/Grow";
 
-// function usePrevious(value: any) {
-//   const ref = React.useRef();
-//   useEffect(() => {
-//     ref.current = value;
-//   });
-//   return ref.current;
-// }
+function usePrevious(value: any) {
+  const ref = React.useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current || [];
+}
 
 interface ItemProps {
   shown: boolean;
   children: any;
   onCompleteOutAnimation?: VoidFunction;
   onExited: VoidFunction;
+  timeout?: { enter?: number; exit?: number };
 }
-function AnimatedListItem({ shown, children, onExited }: ItemProps) {
+function AnimatedListItem({ shown, children, timeout, onExited }: ItemProps) {
   useEffect(() => {}, [shown]);
   return (
-    <Grow in={shown} unmountOnExit={true} onExited={onExited}>
+    <Grow in={shown} onExiting={onExited} timeout={timeout}>
       <div>{children}</div>
     </Grow>
   );
@@ -29,47 +30,80 @@ interface ListProps {
   children: JSX.Element[];
 }
 export const AnimatedList = ({ children }: ListProps) => {
-  // const previousChildren = usePrevious(children);
-  const [hiddenChildren, setHiddenChildren] = useState<any[]>([] as any);
-  const [shownChildren, setShownChildren] = useState<any[]>([] as any);
-
-  const addChildren = () => {
-    const newChildren = children.filter(
-      c => shownChildren.findIndex(sc => sc.key === c.key) === -1
-    );
-    true;
-    setShownChildren([...shownChildren, ...newChildren]);
-  };
+  const previousChildren: any = usePrevious(children);
+  const [removed, setRemoved] = React.useState<{ [index: number]: any }>([]);
+  const [removedShown, setRemovedShown] = React.useState<{
+    [index: number]: any;
+  }>([]);
 
   const removeChildren = () => {
-    const keys: any[] = [];
-    shownChildren.forEach(sc => {
-      if (children.findIndex(c => c.key === sc.key) === -1) {
-        keys.push(sc.key);
-      }
+    const newlyRemoved = previousChildren.filter(
+      (c: any) => children.findIndex(oc => oc.key === c.key) === -1
+    );
+    newlyRemoved.forEach((r: any) => {
+      const index = previousChildren.findIndex((rr: any) => r.key === rr.key);
+      setRemoved({ ...removed, [index]: r });
+      setRemovedShown({ ...removedShown, [index]: r });
+      setTimeout(() => {
+        delete removedShown[index];
+        setRemovedShown({ ...removedShown });
+      }, 100);
     });
-
-    setHiddenChildren([...hiddenChildren, ...keys]);
   };
 
   useEffect(() => {
-    if (shownChildren.length < children.length) {
-      addChildren();
-    } else if (shownChildren.length > children.length) {
+    if (previousChildren.length > children.length) {
       removeChildren();
     }
   }, [children]);
 
-  const handleExit = (key: any) => {
-    console.log("exit", key);
+  const handleExit = (index: any) => {
+    setTimeout(() => {
+      console.log("ttttttttt", removed);
+      delete removed[index];
+      setRemoved({ ...removed });
+    }, 300);
   };
-  return shownChildren.map((Child, i: number) => (
+  return children.length === 0 && removed[0] ? (
     <AnimatedListItem
-      shown={hiddenChildren.indexOf(Child.key) === -1}
-      key={Child.key || i}
-      onExited={() => handleExit(Child.key)}
+      onExited={() => handleExit(0)}
+      key={removed[0].key}
+      shown={removedShown[0] !== undefined}
+      timeout={{ enter: 0 }}
     >
-      {Child}
+      {removed[0]}
     </AnimatedListItem>
-  ));
+  ) : (
+    children.map((Child, i: number) => (
+      <>
+        {i === 0 && removed[i] && (
+          <AnimatedListItem
+            onExited={() => handleExit(i)}
+            key={removed[i].key}
+            shown={removedShown[i] !== undefined}
+            timeout={{ enter: 0 }}
+          >
+            {removed[i]}
+          </AnimatedListItem>
+        )}
+        <AnimatedListItem
+          shown={true}
+          key={Child.key || i}
+          onExited={() => handleExit(Child.key)}
+        >
+          {Child}
+        </AnimatedListItem>
+        {removed[i + 1] && (
+          <AnimatedListItem
+            onExited={() => handleExit(i + 1)}
+            key={removed[i + 1].key}
+            shown={removedShown[i + 1] !== undefined}
+            timeout={{ enter: 0 }}
+          >
+            {removed[i + 1]}
+          </AnimatedListItem>
+        )}
+      </>
+    ))
+  );
 };
